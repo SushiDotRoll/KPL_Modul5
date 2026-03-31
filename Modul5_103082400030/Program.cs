bfd6b41c6807ccb0314d6177b21198ab14bbc7a3
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-03-31 20:47:53 +0700

    menambahkan class SimpleDatabase

--- Modul5_103082400030/Program.cs
+++ Modul5_103082400030/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan p = new Penjumlahan();
+
+        long a = 10;
+        long b = 30;
+        long c = 82;
+
+        long hasil = p.JumlahTigaAngka(a, b, c);
+        Console.WriteLine("Hasil Penjumlahan: " + hasil);
+
+        Console.WriteLine();
+
+        SimpleDataBase<long> db = new SimpleDataBase<long>();
+
+        db.AddNewData(a);
+        db.AddNewData(b);
+        db.AddNewData(c);
+
+        db.PrintAllData();
+    }
+}
---
+++ Modul5_103082400030/SimpleDataBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine(
+                "Data " + (i + 1) +
+                " berisi: " + storedData[i] +
+                ", disimpan pada waktu: " + inputDates[i]
+            );
+        }
+    }
+}
