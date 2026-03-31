7f81bf4f30f433079b30a3b9fc233b31133b1cc2
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-03-31 20:39:17 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Modul5_103082400030/Program.cs
+++ Modul5_103082400030/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan p = new Penjumlahan();
+
+        long a = 10;
+        long b = 30;
+        long c = 82;
+
+        long hasil = p.JumlahTigaAngka(a, b, c);
+
+        Console.WriteLine("Hasil Penjumlahan: " + hasil);
+    }
+}
---
+++ Modul5_103082400030/penjumlahan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+        return x + y + z;
+    }
+}
